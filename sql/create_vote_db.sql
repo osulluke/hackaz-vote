f906736b9033a4d32a10a46ae5ece7cf0dc98da1
Luke O'Sullivan <osulluke@gmail.com>
2017-01-13 23:12:37 -0700

    First sql attempt.
    First attempt to build the database.

--- sql/create_vote_db.sql
+++ sql/create_vote_db.sql
@@ -2,17 +2,17 @@ DROP DATABASE IF EXISTS vote;
 CREATE DATABASE vote;
 USE vote;
 
-CREATE TABLE votes (
+CREATE TABLE votes 
+	(vote_id			INT					NOT NULL,
+     voter_name			CHAR(50)			NOT NULL,
+     plurality_choice	INT					NOT NULL,
+     ranking_choice							NOT NULL,
+     CONSTRAINT			PK_votes			PRIMARY KEY(vote_id))
 
 
-
-)
-
-CREATE TABLE candidate (
-
-
-
-
-
-)
+CREATE TABLE candidate 
+	(candidate_id		INT					NOT NULL,
+     candidate_name		CHAR(50)			NOT NULL,
+     candidate_pic		BLOB				NOT NULL,
+     CONSTRAINT			PK_candidate		PRIMARY KEY(candidate_id))
 
